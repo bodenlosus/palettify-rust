1725350c4f79d94e55501ffb90f7b5aac0640dcd
bodenlosus <johannes.till.schmidt@outlook.de>
2025-01-14 22:14:58 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,59 @@
-/*
-The MIT License (MIT)
+extern crate image;
+extern crate regex;
 
-Copyright (c) 2013-2015, The Gtk-rs Project Developers.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-extern crate gio;
-extern crate glib;
-extern crate gtk;
-
-use gio::prelude::*;
-use glib::clone;
-use gtk::prelude::*;
-use gtk::{IconSize, Orientation, ReliefStyle, Widget};
-
-use std::env::args;
-
-struct Notebook {
-    notebook: gtk::Notebook,
-    tabs: Vec<gtk::Box>,
-}
-
-impl Notebook {
-    fn new() -> Notebook {
-        Notebook {
-            notebook: gtk::Notebook::new(),
-            tabs: Vec::new(),
-        }
-    }
-
-    fn create_tab(&mut self, title: &str, widget: Widget) -> u32 {
-        let close_image = gtk::Image::from_icon_name(Some("window-close"), IconSize::Button);
-        let button = gtk::Button::new();
-        let label = gtk::Label::new(Some(title));
-        let tab = gtk::Box::new(Orientation::Horizontal, 0);
-
-        button.set_relief(ReliefStyle::None);
-        button.set_focus_on_click(false);
-        button.add(&close_image);
-
-        tab.pack_start(&label, false, false, 0);
-        tab.pack_start(&button, false, false, 0);
-        tab.show_all();
-
-        let index = self.notebook.append_page(&widget, Some(&tab));
-
-        button.connect_clicked(clone!(@weak self.notebook as notebook => move |_| {
-            let index = notebook
-                .page_num(&widget)
-                .expect("Couldn't get page_num from notebook");
-            notebook.remove_page(Some(index));
-        }));
-
-        self.tabs.push(tab);
-
-        index
-    }
-}
-
-fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
-
-    window.set_title("Notebook");
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(640, 480);
-
-    let mut notebook = Notebook::new();
-
-    for i in 1..4 {
-        let title = format!("sheet {}", i);
-        let label = gtk::Label::new(Some(&*title));
-        notebook.create_tab(&title, label.upcast());
-    }
-
-    window.add(&notebook.notebook);
-    window.show_all();
-}
+use regex::Regex;
+use image::{ImageBuffer, Rgb, ImageReader};
+use std::{fmt::Debug, fs};
 
 fn main() {
-    let application = gtk::Application::new(
-        Some("com.github.gtk-rs.examples.notebook"),
-        Default::default(),
-    )
-    .expect("Initialization failed...");
+    // Create an 800x600 image with black pixels
+    let dyn_img = match ImageReader::open("./RGB_24bits_palette_R255.png") {
+        Ok(r) => r,
+        Err(e) => panic!("Error opening image: {}", e),
+    };
+    let mut img = dyn_img.decode().unwrap().to_rgb8();
 
-    application.connect_activate(|app| {
-        build_ui(app);
-    });
+    // Set each pixel to a color
+    for (x, y, pixel) in img.enumerate_pixels_mut() {          // Blue channel
+        let r = (pixel[0] / 2) as u8;
+        let g = (pixel[1] / 2) as u8;
+        let b = (pixel[2] / 2) as u8;
+        *pixel = Rgb([r, g, b]);
+    }
 
-    application.run(&args().collect::<Vec<_>>());
+    // Save the image to a file
+    img.save("output.png").unwrap();
+    read_palette("/home/johannes/Pictures/horizon_theme");
+}
+
+fn read_palette(path: &str) -> Vec<[u8; 3]>{
+    println!("Reading palette {path}");
+    let content = match fs::read_to_string(path) {
+        Ok(string) => string,
+        Err(e) => panic!("Error reading palette: {}", e),
+    };
+    let palette: Vec<[u8; 3]> = content
+        .split("\n")
+        .map(|hex_code| parse_hex_str(hex_code).unwrap()) // Here we map each line to the same hex code
+        .collect();
+    return palette;
+}
+
+fn parse_hex_str(str: &str) -> Result<[u8; 3], String> {
+    let pat = Regex::new("^#[0-9a-fA-F]{6}$").unwrap();
+    if !pat.is_match(str) {
+        return Err(format!("{} is not a valid hex-color", str));
+    }
+    let r = u8::from_str_radix(&str[1..3], 16)
+        .map_err(|_| "Invalid red value")
+        .unwrap();
+    let g = u8::from_str_radix(&str[3..5], 16)
+        .map_err(|_| "Invalid green value")
+        .unwrap();
+    let b = u8::from_str_radix(&str[5..7], 16)
+        .map_err(|_| "Invalid blue value")
+        .unwrap();
+
+    println!("{r}, {g}, {b}");
+    Ok([r, g, b])   // Convert the hex string to a RGB color
 }
