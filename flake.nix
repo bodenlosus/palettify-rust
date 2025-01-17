2904b4f1287d95d6d2017421fe2f2e3e4f26a2eb
bodenlosus <johannes.till.schmidt@outlook.de>
2025-01-17 16:33:47 +0100

    Update flake.nix to enhance Rust development environment and add support for macOS systems

--- flake.nix
+++ flake.nix
@@ -1,38 +1,32 @@
 {
-  description = "A very basic flake";
+  description = "A Nix-flake-based Rust development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-    
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-      with nixpkgs.legacyPackages.${system};
-      let
-        pkg = pkgs.callPackage ./. {};
-        rustToolchain =
-          let
-            rust = pkgs.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [ "rust-src" "rustfmt" ];
-            };
 
-      in {
-        packages.default = pkg;
-        devShells.default = mkShell {
+  outputs = { self, nixpkgs, rust-overlay, flake-utils}:
+  flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+    let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+    in
+    rec{
+      overlays.default = final: prev: {
+        rustToolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      };
+      packages.default = pkgs.callPackage ./. {};
+      devShells.${system}.default = pkgs.mkShell {
           packages = with pkgs; [
             rustToolchain
+            openssl
             pkg-config
             cargo-deny
             cargo-edit
@@ -42,7 +36,7 @@
 
           env = {
             # Required by rust-analyzer
-            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
           };
         };
       });
